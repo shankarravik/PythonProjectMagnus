ca6fadb0c109a2528aa7cb9b3c4a11235594c517
shankarravik <shankarravik@yahoo.co.in>
2023-07-26 20:31:21 -0500

    Day4_26_July

---
+++ List Exmp.py
@@ -0,0 +1,17 @@
+a1 = [10,20,30,'welcome',50,60,'Hello']
+
+print (a1)
+print (a1[5])
+print (a1[0:5])
+print (a1[:5])
+print (a1[0:])
+print (a1)
+a1.append(800) # Appending the values (only single value)
+print (a1)
+a1.insert(4,'Hi') # Inserting the values in desired range
+print (a1)
+a1[7] = "Hola" # To replace a desired value
+print (a1)
+a1.remove('welcome')
+print (a1)
+
