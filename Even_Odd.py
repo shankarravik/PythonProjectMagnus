f83f8d94e7e39c7452538187dc5bf74c07c8f8d3
shankarravik <shankarravik@yahoo.co.in>
2023-08-01 00:05:30 -0500

    Even Odd Code

---
+++ Even_Odd.py
@@ -0,0 +1,8 @@
+# Write a program to check if a Number is Odd or Even. Take number as a input from user at runtime.
+
+n=int(input("Enter a number:"))
+if n%2==0:
+    print("You have entered an even number")
+else:
+    print("You have entered an odd number")
+
